1d6402d79f70277b3197dd34e1c6c7648ece03cf
Aiman Naeem <109482563+Aimancod01@users.noreply.github.com>
2025-07-06 16:53:06 +0500

    new changes in cart

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ function App() {
   }
   return (
     <CartProvider>
-      {show && <div>Cart...</div>}
+      {show && <div>C...</div>}
       <Header onShowCart={showCartHandler} />
       <main>
         <Meals />
